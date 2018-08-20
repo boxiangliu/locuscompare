e948be26a725ba831bcd70160497f88a4b35b53e
jollier.liu@gmail.com <jollier.liu@gmail.com>
2018-08-20 15:34:43 -0700

    modified:   utils/mysql/coloc2mysql.R

--- utils/mysql/coloc2mysql.R
+++ utils/mysql/coloc2mysql.R
@@ -1,7 +1,12 @@
 library(data.table)
 library(stringr)
+library(RMySQL)
+library(pool)
+library(DBI)
+source('config/config.R')
 
 in_fn = '/srv/persistent/bliu2/rpe/processed_data/finemap/manhattan/2018-06-23_21-47-08_rpe_amd_gtex/Fritsche_sorted_txt_gz_finemap_clpp_status.txt'
+
 
 read_colocalization = function(fn){
     x = fread(fn)
@@ -21,7 +26,7 @@ munge_colocalization = function(x){
 	return(y)
 }
 
-create_table = function(){
+create_table = function(table_name){
 	message('INFO - creating table...')
 	if (dbExistsTable(locuscompare_pool,table_name)) {
 		dbRemoveTable(locuscompare_pool,table_name)
@@ -32,20 +37,61 @@ create_table = function(){
 		statement = sprintf("create table %s
 		(%s_id int auto_increment primary key,
 		gwas varchar(100),
-		rsid varchar(100),
-		pval double);",table_name,table_name)
+		trait varchar(64),
+		eqtl varchar(64),
+		gene_id varchar(20),
+		clpp double);",table_name,table_name)
 		)
 }
 
-upload_table = function(){
-    
+upload_table = function(colocalization,table_name){
+	message('INFO - uploading GWAS')
+	step = 1e6
+	total_row = nrow(colocalization)
+	for (i in seq(1,total_row,step)) {
+		start = i
+		print(start)
+		end = ifelse(i+step-1<=total_row,i+step-1,total_row)
+		fwrite(colocalization[start:end,],'coloc_tmp.csv')
+
+		dbExecute(
+			conn = locuscompare_pool,
+			statement = sprintf("load data local infile 'coloc_tmp.csv' 
+			into table %s
+			fields terminated by ','
+			lines terminated by '\n'
+			ignore 1 lines
+			(gwas, trait, eqtl, gene_id, clpp);",table_name)
+			)
+
+		unlink('coloc_tmp.csv')
+	}
 }
 
+index_table = function(table_name){
+	message('INFO - indexing table')
+	dbExecute(
+		conn = locuscompare_pool,
+		statement = sprintf('alter table %s
+			add index gwas (gwas),
+			add index trait (trait),
+			add index eqtl (eqtl),
+			add index gene_id (gene_id)',table_name)
+		)
+}
+
+locuscompare_pool = dbPool(
+	drv = RMySQL::MySQL(), 
+	dbname = 'locuscompare',
+	host = aws_host,
+	username = aws_username,
+	password = aws_password
+	)
 
 colocalization = read_colocalization(in_fn)
 colocalization = munge_colocalization(colocalization)
 
 table_name = 'eCAVIAR'
 create_table(table_name)
-upload_table(table_name)
-
+upload_table(colocalization,table_name)
+index_table(table_name)
